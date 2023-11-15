326316d08f07c40aba827e4ef3c608e54094a21b
ameldoglod <ameldoglod7@gmail.com>
2023-11-15 12:44:48 +0100

    Zadatak

---
+++ src/main/java/src/Main.java
@@ -0,0 +1,9 @@
+package src;
+
+public class Main {
+    public static void main(String[] args) {
+        double poluprecnik = 10;
+        double visina = 50;
+        double zapremina  = Math.PI * poluprecnik * poluprecnik * visina ;
+        System.out.println("Zapremina valjka je : " + zapremina);
+    }}
